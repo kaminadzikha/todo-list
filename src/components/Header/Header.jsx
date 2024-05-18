51ed94eef8cf51eb5d984f7e8f2ea88b3c91fd58
PaJlma <jmaster21042005@mail.ru>
2024-05-18 21:37:50 +0300

    added big-balls

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,8 @@
-import styles from './Header.module.scss'
+import styles from "./Header.module.scss";
 
 export const Header = () => (
-  <div className={styles.header}>Мои задачи</div>
-)
+  <div className={styles.header}>
+    Мои задачи (БОЛЬШИЕ-БОЛЬШИЕ-ПРЕБОЛЬШИЕ ЯЙЦА)
+  </div>
+);
+
